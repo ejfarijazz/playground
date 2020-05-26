4d65303de4d47f710e94026a772abccfbd1cfc6a
ejfarijazz <ejfarijazz@mail.ru>
2020-05-26 09:30:28 +0300

    initial

---
+++ basics/basic-syntax.js
@@ -0,0 +1,46 @@
+// comments
+// one line
+
+// variables
+/*
+Multi
+line
+comment
+*/
+
+var a = 1; // a variable
+var b = 2;
+
+var c = a + b;
+c = b + 3;
+
+console.log("c = " + c);
+
+// data types
+// integer numbers
+var i = 5;
+// float number
+var d = 5.43;
+// strings
+var s1 = "Hello, "
+var s2 = "world!"
+var s = s1 + s2
+var strangeBehaviour = s1 + i;
+// boolean
+var t = true;
+var f = false;
+// and something more...
+
+// complicated logic
+
+function complicatedLogic(q, w, e) {
+    var g = q + w;
+    var f = g - e;
+    return f * g / 12;
+}
+
+var result = complicatedLogic(3, 4, 5);
+
+function S(a, b) {
+    return a * b;
+}
